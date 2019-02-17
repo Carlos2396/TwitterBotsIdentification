2a1762e7fd376374067270f1e51a32791857ed5b
Carlos2396 <carlosaam96@gmail.com>
2019-02-17 14:32:52 -0600

    Generated arff files only with emotion features.

---
+++ parseResultsToKaggle.py
@@ -0,0 +1,28 @@
+import csv, sys
+
+def main():
+    if len(sys.argv) != 3:
+        return
+
+    in_name = sys.argv[1]
+    out_name = sys.argv[2]
+
+    with open(in_name, mode='r') as read_file:
+        reader = csv.DictReader(read_file)
+        tweets = []
+
+        for row in reader:
+            data = {}
+            data['Id'] = row['inst#']
+            data['Expected'] = 1 if (row['predicted']) == "1:Bot" else 0
+            tweets.append(data)
+
+        with open(out_name, 'w') as csvFile:
+            writer = csv.DictWriter(csvFile, tweets[0].keys())
+            writer.writeheader()
+
+            for tweet in tweets:
+                writer.writerow(tweet)
+
+if __name__ == '__main__':
+    main()
