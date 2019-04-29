d703b1335606ecc3079ee0c047686d099c588b57
Carlos2396 <carlosaam96@gmail.com>
2019-04-29 18:41:16 -0500

    Extracted sentiment from non english langs

---
+++ Features/positiveness.py
@@ -0,0 +1,33 @@
+import indicoio, json, sys
+
+indicoio.config.api_key = '9f1dbd8b60857ed8ac6c10e441772edc'
+
+def main():
+    if len(sys.argv) != 3:
+        return
+        
+    inname = sys.argv[1]
+    outname = sys.argv[2]
+
+    with open(inname, mode='r') as inFile:
+        tweets = json.load(inFile)
+        count = 0
+
+        for tweet in tweets:
+            if tweet['lang'] == 'en':
+                tweet['positiveness'] = float(indicoio.sentiment_hq(tweet['text']))
+            else:
+                tweet['positiveness'] = None
+
+            count += 1
+
+            if count % 100 == 0:
+                print(count)
+                with open(outname, 'w') as outfile:
+                    json.dump(tweets, outfile)
+        
+        with open(outname, 'w') as outfile:
+            json.dump(tweets, outfile)
+
+if __name__ == '__main__':
+    main()
